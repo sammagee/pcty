2d3e850d3bd1a0eb3ac39c6fc75e92aaafad7202
Sam <s@sammagee.me>
2021-04-23 13:52:57 -0500

    updates pagination for smaller screens

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -1,9 +1,11 @@
 <template>
     <div v-if="links.length > 3">
-        <div class="flex items-center justify-center">
+        <div class="flex items-center justify-between md:justify-center">
             <template v-for="(link, key) in links" :key="key">
-                <div v-if="!link.url" class="inline-flex items-center justify-center h-10 min-w-[2.5rem] mb-1 mr-1 text-sm leading-4 text-gray-300 rounded-lg" v-html="link.label" />
-                <inertia-link v-else class="inline-flex items-center justify-center h-10 min-w-[2.5rem] mb-1 mr-1 text-sm leading-4 rounded-lg text-gray-500 hover:bg-white focus:border-orange-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-orange-300" :class="[link.active && 'shadow bg-white']" :href="link.url" v-html="link.label" />
+                <div :class="{'hidden md:block': key !== 0 && key !== links.length - 1}">
+                    <div v-if="!link.url" class="inline-flex items-center justify-center h-10 min-w-[2.5rem] mb-1 mr-1 text-sm leading-4 text-gray-300 rounded-lg" v-html="link.label" />
+                    <inertia-link v-else class="inline-flex items-center justify-center h-10 min-w-[2.5rem] mb-1 mr-1 text-sm leading-4 rounded-lg text-gray-500 hover:bg-white focus:border-orange-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-orange-300" :class="[link.active && 'shadow bg-white']" :href="link.url" v-html="link.label" />
+                </div>
             </template>
         </div>
     </div>
