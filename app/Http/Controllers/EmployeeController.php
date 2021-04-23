df2455d72f66de24a726b310cda3df5e7302d393
Sam <s@sammagee.me>
2021-04-23 09:25:26 -0500

    fix orderBy

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use App\Models\Team;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
@@ -23,24 +24,7 @@ class EmployeeController extends Controller
 
         return Inertia::render('Employees', [
             'search' => $request->query('query'),
-            'employees' => Employee::search($request->query('query'),)
-                ->where('team_id', $team->id)
-                ->orderBy('name')
-                ->query(fn ($employee) => $employee->with('dependents'))
-                ->paginate()
-                ->withQueryString()
-                ->through(fn ($employee) => [
-                    'id' => $employee->id,
-                    'name' => $employee->name,
-                    'gross_pay_per_period' => $employee->gross_pay / 26 / 100,
-                    'gross_pay_per_year' => $employee->gross_pay / 100,
-                    'benefit_cost_per_period' => $employee->benefit_cost / 26 / 100,
-                    'benefit_cost_per_year' => $employee->benefit_cost / 100,
-                    'net_pay_per_period' => $employee->gross_pay / 26 / 100 - $employee->benefit_cost / 26 / 100,
-                    'net_pay_per_year' => $employee->gross_pay / 100 - $employee->benefit_cost / 100,
-                    'profile_photo_url' => $employee->profile_photo_url,
-                    'dependents' => $employee->dependents,
-                ]),
+            'employees' => $request->query('query') ? $this->search($request, $team) : $this->fetch($team),
             'average' => ($employeesCount > 1 ? $totalBenefitCost / $employeesCount : $totalBenefitCost) / 100,
             'total' => $totalBenefitCost / 100,
         ]);
@@ -91,7 +75,50 @@ class EmployeeController extends Controller
         return redirect()->back();
     }
 
-    protected function updateDependents(Employee $employee, array $dependents) {
+    protected function fetch(Team $team)
+    {
+        return Employee::where('team_id', $team->id)
+            ->with('dependents')
+            ->orderBy('name')
+            ->paginate()
+            ->withQueryString()
+            ->through(fn ($employee) => [
+                'id' => $employee->id,
+                'name' => $employee->name,
+                'gross_pay_per_period' => $employee->gross_pay / 26 / 100,
+                'gross_pay_per_year' => $employee->gross_pay / 100,
+                'benefit_cost_per_period' => $employee->benefit_cost / 26 / 100,
+                'benefit_cost_per_year' => $employee->benefit_cost / 100,
+                'net_pay_per_period' => $employee->gross_pay / 26 / 100 - $employee->benefit_cost / 26 / 100,
+                'net_pay_per_year' => $employee->gross_pay / 100 - $employee->benefit_cost / 100,
+                'profile_photo_url' => $employee->profile_photo_url,
+                'dependents' => $employee->dependents,
+            ]);
+    }
+
+    protected function search(Request $request, Team $team)
+    {
+        return Employee::search($request->query('query'))
+            ->where('team_id', $team->id)
+            ->query(fn ($employee) => $employee->with('dependents'))
+            ->paginate()
+            ->withQueryString()
+            ->through(fn ($employee) => [
+                'id' => $employee->id,
+                'name' => $employee->name,
+                'gross_pay_per_period' => $employee->gross_pay / 26 / 100,
+                'gross_pay_per_year' => $employee->gross_pay / 100,
+                'benefit_cost_per_period' => $employee->benefit_cost / 26 / 100,
+                'benefit_cost_per_year' => $employee->benefit_cost / 100,
+                'net_pay_per_period' => $employee->gross_pay / 26 / 100 - $employee->benefit_cost / 26 / 100,
+                'net_pay_per_year' => $employee->gross_pay / 100 - $employee->benefit_cost / 100,
+                'profile_photo_url' => $employee->profile_photo_url,
+                'dependents' => $employee->dependents,
+            ]);
+    }
+
+    protected function updateDependents(Employee $employee, array $dependents)
+    {
         $employee->dependents()->delete();
 
         return collect($dependents)->map(function ($dependent) use ($employee) {
@@ -103,25 +130,29 @@ class EmployeeController extends Controller
         })->reduce(fn($sum, $dependent) => $sum + $this->benefitCost(500, $dependent['name']));
     }
 
-    protected function benefitCost(int $amount, $name) {
+    protected function benefitCost(int $amount, $name)
+    {
         $multiplier = strtoupper(substr($name, 0, 1)) === 'A' ? 0.9 : 1.0;
 
         return $amount * $multiplier * 100;
     }
 
-    protected function incrementTotalBenefitCost($id, int $amount) {
+    protected function incrementTotalBenefitCost($id, int $amount)
+    {
         $key = 'total_benefit_cost:'.$id;
         if (Cache::has($key)) Cache::increment($key, $amount);
         else Cache::forever($key, $amount);
     }
 
-    protected function decrementTotalBenefitCost($id, int $amount) {
+    protected function decrementTotalBenefitCost($id, int $amount)
+    {
         $key = 'total_benefit_cost:'.$id;
         if (Cache::has($key)) Cache::decrement($key, $amount);
         else Cache::forever($key, 0);
     }
 
-    protected function getTotalBenefitCost($team) {
+    protected function getTotalBenefitCost($team)
+    {
         if (Cache::has('total_benefit_cost:'.$team->id)) return Cache::get('total_benefit_cost:'.$team->id);
         
         $total = $team->employees->reduce(fn($sum, $employee) => $sum + $this->benefitCost(1000, $employee['name']));
